1c2007f310156a0e5ac17b12d3bbfb91346e3b00
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-04-21 11:45:17 -0500

    intiial

---
+++ src/components/main/MainDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MainDisplay = () => {
+    return <div className="main-container">
+        <h3>MAIN DISPLAY</h3>
+    </div>
+}
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Sidebar = () => {
+    return <div className="sidebar-container">
+        <h3>SIDEBAR</h3>
+    </div>
+}
