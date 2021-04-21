078a73da926f14f984d189ce461aeff7a225d285
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-04-21 15:21:52 -0500

    sidebar started

--- src/components/main/MainDisplay.js
+++ src/components/main/MainDisplay.js
@@ -7,14 +7,17 @@ const {blogs, getBlogs} = useContext(BlogContext)
 const [latestBlog, setLatestBlog] = useState({})
 
 useEffect(() => {
-    getBlogs().then(setLatestBlog(blogs[0]))
-    console.log(latestBlog)
+    getBlogs()
 },[])
 
+useEffect(() => {
+    setLatestBlog(blogs[0])
+},[blogs])
+
     return <div className="main-container">
-        <h3>MAIN DISPLAY</h3>
+        {/* <h3>MAIN DISPLAY</h3>
             <p>{latestBlog.title}</p>
             <p>{latestBlog.blogBody}</p>
-            <img src={latestBlog.imageURL}/>
+            <img className="mainDisplay-img" src={latestBlog.imageURL}/> */}
     </div>
 }
