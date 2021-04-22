61ee3c959f7b32d2921483043b6841ed2a599f3b
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-04-22 11:42:35 -0500

    form submits to json

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Blog } from './Blog.js'
 import { Route } from "react-router-dom"
+import {Form} from './components/form/Form.js'
 import {BlogProvider} from './components/BlogProvider.js'
 
 export const ApplicationViews = (props) => {
@@ -8,6 +9,7 @@ export const ApplicationViews = (props) => {
     <BlogProvider>
       <Route exact path="/" render={(props) => <Blog {...props} />} />
       <Route path="/:BlogId(\d+)" render={(props) => <Blog {...props} />}/> 
+      <Route exact path="/form" render={(props) => <Form {...props} />} />
     </BlogProvider>
     </>
 }
